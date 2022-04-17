0da1f8e659729fc54735d0c8e4fd68ae706fa4bb
shenyuchen <15200831505@163.com>
2022-04-17 11:55:24 +0800

    1、随机抽牌 random.choice() 2、按照规则从小到大给整个牌组排序

--- fluent_python_2017/chapter_1/Cards.py
+++ fluent_python_2017/chapter_1/Cards.py
@@ -3,11 +3,12 @@
 @time   2022-04-17 10:52
 
 spades      ♠
+hearts      ❤
 diamonds    ♦
 clubs       ♣
-hearts      ❤
 """
 import collections
+from random import choice
 
 
 """
@@ -22,7 +23,7 @@ class FrenchDeck:
     构建一副扑克牌
     """
     ranks = [str(i) for i in range(2, 11)] + list('JQKA')
-    suits = '♠ ♦ ♣ ❤'.split()
+    suits = 'spades hearts diamonds clubs'.split()
 
     def __init__(self):
         self._cards = [Card(rank, suit) for rank in self.ranks for suit in self.suits]
@@ -41,4 +42,26 @@ if __name__ == '__main__':
     deck = FrenchDeck()
     # print(len(deck))
 
-    print(deck[0], deck[-1])
+    # print(deck[0], deck[-1])
+
+    # print(choice(deck))     # 随机抽牌
+    # print(choice(deck))     # 随机抽牌
+
+    # for card in reversed(deck):
+    #     print(card)
+
+    """
+    给扑克的每个花色赋予值：
+    最大  spades      ♠
+    其次  hearts      ❤
+    次之  diamonds    ♦
+    最小  clubs       ♣
+    """
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+    def spades_high(card):
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit]
+
+    for card in sorted(deck, key=spades_high):  # 输出排好序的扑克
+        print(card)
