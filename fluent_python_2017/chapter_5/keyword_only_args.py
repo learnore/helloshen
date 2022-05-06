073743938060dd6a4d3012b9cc578a740d92c059
shenyuchen <15200831505@163.com>
2022-05-06 08:34:48 +0800

    1、函数内省  dir(function) 2、从定位参数到仅限关键字参数

---
+++ fluent_python_2017/chapter_5/keyword_only_args.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+  IDE         : PyCharm
+  File Name   : keyword_only_args
+  Description : 从定位参数到仅限关键字参数
+  Author      : chenyushencc@gmail.com
+  date        : 2022/5/6 8:19
+-------------------------------------------------
+"""
+
+
+def tag(name, *content, cls=None, **attrs):
+    """生成一个或多个 HTML 标签"""
+    if cls is not None:
+        attrs['class'] = cls
+
+    if attrs:
+        attr_str = ''.join(' %s="%s"' % (attr, value)
+                           for attr, value in sorted(attrs.items()))
+    else:
+        attr_str = ''
+
+    if content:
+        return '\n'.join('<%s%s>%s</%s>' %
+                         (name, attr_str, c, name) for c in content)
+    else:
+        return '<%s%s />' % (name, attr_str)
+
+
+if __name__ == '__main__':
+    print(tag('br'))                                    # <br />
+    print(tag('p', 'hello'))                            # <p>hello</p>
+    print(tag('p', 'hello', 'world'))
+    print(tag('p', 'hello', id=33))                     # <p id="33">hello</p>
+    print(tag('p', 'hello', 'world', cls='sidebar'))    # <p class="sidebar">hello</p>      <p class="sidebar">world</p>
+    print(tag(content='testing', name='img'))           # <img content="testing" />
+    my_tag = {
+        'name': 'img',
+        'title': 'Sunset Boulevard',
+        'src': 'sunset.jpg',
+        'cls': 'framed'
+    }
+    print(tag(**my_tag))        # <img class="framed" src="sunset.jpg" title="Sunset Boulevard" />
--- fluent_python_2017/chapter_5/my_functions.py
+++ fluent_python_2017/chapter_5/my_functions.py
@@ -70,3 +70,25 @@ if __name__ == '__main__':
     """
     print(all([]))
     print(any([]))
+
+    """
+    * 函数内省  dir(function)
+    * ['__annotations__', '__builtins__', '__call__', '__class__', '__closure__', '__code__', '__defaults__', 
+    * '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__get__', 
+    * '__getattribute__', '__globals__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__kwdefaults__', 
+    * '__le__', '__lt__', '__module__', '__name__', '__ne__', '__new__', '__qualname__', '__reduce__', 
+    * '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__']
+    """
+    print(dir(factorial))
+
+    """
+    * 计算差集，然后排序，得到类的实例没有而函数有的属性列表
+    """
+    class C: pass
+    obj = C()
+    def func(): pass
+    """
+    * ['__annotations__', '__builtins__', '__call__', '__closure__', '__code__', '__defaults__', 
+    * '__get__', '__globals__', '__kwdefaults__', '__name__', '__qualname__']
+    """
+    print(sorted(set(dir(func)) - set(dir(obj))))
