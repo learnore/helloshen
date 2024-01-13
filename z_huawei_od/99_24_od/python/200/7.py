e7dbc8bf09e46f419c78288d348fecf7e119e18d
shenyuchen <learnore@163.com>
2024-01-13 17:37:02 +0800

    Cause: C卷 T7 [200] pass Coder: shenyuchen Email: chenyushencc@gamil.com

--- z_huawei_od/99_24_od/python/200/7.py
+++ z_huawei_od/99_24_od/python/200/7.py
@@ -27,45 +27,39 @@ def bfs_1(graph, mom, baby, n):
     baby_x, baby_y = baby
 
     queue = Queue()
-    steps, candies = 0, 0                           # 默认步数和糖果都是 0
-    queue.put((mom_x, mom_y, steps, candies))       # 将 mom 的初始状态首次加入
+    queue.put((mom_x, mom_y, 0, 0))       # 将 mom 的初始状态首次加入，默认步数和糖果都是 0
 
     visited = set()
+    visited.add(mom)
     while not queue.empty():
         temp_x, temp_y, temp_steps, temp_candies = queue.get()
-        if (temp_x, temp_y) in visited:     # 如果已经该坐标已经被访问，那就跳过本次 while 循环
-            continue
-        else:
-            visited.add((temp_x, temp_y))  # 记录当前经过的位置
-
+        if temp_x == baby_x and temp_y == baby_y:               # 如果找到了孩子，那就看看当前获得的糖果数，取最大的
+            max_candy = max(temp_candies, temp_candies)
+            continue            # 找到了也就不用继续移动
 
         # 从当前的位置，继续上下左右移动
         for d in directions:
             temp_x += d[0]
             temp_y += d[1]
 
-            if temp_x < 0 or temp_y < 0 or temp_x >= n or temp_y >= n:        # 越界
-                temp_x -= d[0]          # 恢复
-                temp_y -= d[1]          # 恢复
+            if temp_x < 0 or temp_y < 0 or temp_x >= n or temp_y >= n \
+                    or graph[temp_x][temp_y] == -1 or (temp_x, temp_y) in visited:        # 越界、障碍物、已被访问
+                temp_x -= d[0]                  # 易错，要恢复
+                temp_y -= d[1]                  # 易错，要恢复
                 continue
 
-            if graph[temp_x][temp_y] == -1:         # 是障碍物
-                continue
-
-            if temp_x == baby_x and temp_y == baby_y:       # 找到孩子，存储游戏档案
-                continue
-
+            visited.add((temp_x, temp_y))           # 访问前加入
             temp_candies += graph[temp_x][temp_y]
-            max_candy = max(temp_candies, temp_candies)
-
-            steps = temp_steps + 1
-            queue.put((temp_x, temp_y, steps, max_candy))
+            queue.put((temp_x, temp_y, temp_steps + 1, temp_candies))
 
     return max_candy
 
 
 def bfs_2(graph, mom, baby, n):
-    """ 知道起点和终点，那就可以双向奔赴，母亲和孩子都前进 """
+    """
+    # 知道起点和终点，那就可以双向奔赴，母亲和孩子都前进
+    # 起点和终点轮流扩散
+    """
     max_candy = -1
 
     return max_candy
@@ -88,6 +82,6 @@ if __name__ == "__main__":
 
         graph.append(line)
 
-
-    max_candies = bfs_1(graph, mom, baby, n)
+    # max_candies = bfs_1(graph, mom, baby, n)
+    max_candies = bfs_2(graph, mom, baby, n)
     print(max_candies)
