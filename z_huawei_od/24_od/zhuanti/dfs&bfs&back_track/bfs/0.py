4c9d034429304a95a114b8bd73a00158a4e2a73a
shenyuchen <learnore@163.com>
2024-01-23 17:06:48 +0800

    Cause: just want to commit Coder: shenyuchen Email: chenyushencc@gamil.com

--- z_huawei_od/24_od/zhuanti/dfs&bfs&back_track/bfs/0.py
+++ z_huawei_od/24_od/zhuanti/dfs&bfs&back_track/bfs/0.py
@@ -19,13 +19,13 @@ def bfs(graph, mom, baby, n):
     # todo 初始化参数
     result = 0
 
-    queue = Queue()
-    queue.put( )       # 将 mom 的初始状态首次加入，默认步数和糖果都是 0
+    queue = Queue()                 # ******
+    queue.put( )
 
-    visited = set()
-    visited.add( )      # 加入首次的参数
-    while not queue.empty():
-        temp_x, temp_y, temp_steps, temp_candies = queue.get()
+    visited = set()                 # ******
+    visited.add( )                  # 加入首次的参数
+    while not queue.empty():        # ******
+        temp_x, temp_y, temp_steps, temp_candies = queue.get()          # ******
         # todo 逻辑
 
     return result
