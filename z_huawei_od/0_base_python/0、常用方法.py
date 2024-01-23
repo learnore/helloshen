1f2ed8c3baaf84b885ae3a1756a45514a9454519
shenyuchen <learnore@163.com>
2024-01-23 20:16:27 +0800

    Cause: 回溯总结 Coder: shenyuchen Email: chenyushencc@gamil.com

--- z_huawei_od/0_base_python/0、常用方法.py
+++ z_huawei_od/0_base_python/0、常用方法.py
@@ -20,6 +20,16 @@ def check_even_odd(number):
         print(f"{number} 是奇数")
 
 
+def array_catch_the_same(arrays):
+    """ [[], []. []]数组去重 """
+    """ tuple - set - list """
+    result_set = set(tuple(i) for i in arrays)      # 先将内部的数组 tuple([])，再用 set 去重
+    result = [list(s) for s in result_set]          # 恢复成数组
+
+    # [list(i) for i in set(tuple(i) for i in arrays)]
+    return result
+
+
 if __name__ == "__main__":
     check_even_odd(7)
 
