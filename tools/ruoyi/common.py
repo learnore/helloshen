d1fdc5c5f86b7ab095211f3c4678226a2b0f4801
shenli <15200831505@163.com>
2024-05-07 20:48:14 +0800

    1、headers 作为 global 变量 2、新增任务状态，task_status 3、新增登录接口 + 获取任务接口

--- tools/ruoyi/common.py
+++ tools/ruoyi/common.py
@@ -16,39 +16,88 @@ import json
 
 
 http_url = "https://book-video-admin.eebbk.net"
-
-token = 'your_token_here'
-
 captcha_image_interface = "/book-video-admin/captchaImage"    # 获取验证码的图片接口
-login_interface = "/book-video-admin/login"
+login_interface = "/book-video-admin/login"                   # 登录
+task_interface = "/book-video-admin/bookVideo/videoTask/list"       # 获取任务
 
 
-def get_request(interface_name, headers=None):
+# token = 'your_token_here'
+headers = {'Content-Type': 'application/json'}      # 设置请求头，指定 Content-Type 为 application/json
+
+# 任务状态
+task_status = {
+    3: "已上传",
+    5: "审核待指派",
+    6: "待审核",
+    7: "审核问题待确认",
+    8: "审核不通过",
+    9: "品检待分配",
+    10: "品检待指派",
+    11: "待品检",
+    12: "品检问题待确认",
+    13: "品检不通过",
+    14: "待稽核",
+    15: "稽核通过",
+    16: "稽核不通过",
+    17: "免品检",
+    18: "免稽核",
+    19: "待效果审核",
+    20: "废弃待确认",
+    21: "废弃通过",
+    22: "废弃驳回",
+    23: "审核申诉",
+    24: "审核申诉不通过",
+    25: "品检申诉",
+    26: "品检申诉不通过",
+    27: "品检分配退回",
+    28: "效果审核通过",
+    29: "效果审核不通过",
+    30: "待效果品检",
+    31: "效果品检中",
+    32: "效果品检合格",
+    33: "效果品检不合格",
+    34: "免效果品检",
+    35: "效果打回待确定",
+    95: "超时回收",
+    98: "错题确认中",
+    99: "待制作"
+}
+
+
+def get_request(interface_name, params=None):
     """ get 请求公共方法 """
-    response = requests.get(http_url + interface_name)  # 发送GET请求
-    response_string = response.text  # response.text 是一个包含JSON格式数据的字符串
+    print(f"headers: {headers}, params: {params}")
+
+    response = requests.get(http_url + interface_name, headers=headers, params=params)  # 发送GET请求
 
     print(f"{interface_name}: {response.status_code}")      # 打印状态码
-    return json.loads(response_string)  # 将JSON字符串解析为Python字典
+    return json.loads(response.text)
 
 
 def post_request(interface_name, **data):
     """ post 请求公共方法 """
+    print(f"headers: {headers}")
 
-    # data = {'key': 'value'}
-    response = requests.post(http_url + interface_name, data=data)
+    json_data = json.dumps(data)
+    response = requests.post(http_url + interface_name, data=json_data, headers=headers)
+
     print(f"{interface_name}: {response.status_code}")      # 打印状态码
+    return json.loads(response.text)
 
 
+def post_headers(new_token):
+    """ 修改 headers，加入 token """
+    global headers
+    headers = {
+        'Authorization': f'Bearer {new_token}',
+        'Content-Type': 'application/json'
+    }
+    return headers
 
-def post_token(new_token):
-    """ 修改 token """
-    token = new_token
 
-
-def get_token():
-    """ 获取 token """
-    return token
+def get_headers():
+    """ 获取 headers """
+    return headers
 
 
 if __name__ == "__main__":
